795ddf1e05ffb45efd45b49f2a83347a748e76fb
Ouadie <elamraniouadie@gmail.com>
2023-12-29 12:04:17 +0100

    creation of menu action choice in Main class

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,14 +1,49 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Services serv = new Services();
-
-		serv.viewAllEmployees();
-        serv.deleteEmployeeById();
-        serv.updateEmployee();
+        Scanner in = new Scanner(System.in);
+        int choice;
+        
+        System.out.println("Welcome to Employee System Management : ");
+        System.out.println("#-------------------#");
+       
+        // Menu for user to choose what to do
+       
+        while(1>0) {
+        	 System.out.println("Please choose an action : ");  
+             System.out.println("1 - View employees list.\n2 - Search employee.\n3 - Add new employee.\n4 - Update employee.\n5 - Delete employee.\n6 - Exit");
+        	choice = in.nextInt();
+        	switch(choice) {
+        	case 1 :
+        		serv.viewAllEmployees();
+        		break;
+        	case 2 :
+        		serv.searchEmployeeById();
+        		break;
+        	case 3 :
+        		serv.AddEmployees();
+        		break;
+        	case 4 :
+        		serv.updateEmployee();
+        		break;
+        	case 5 :
+        		serv.deleteEmployeeById();
+        		break;
+        	case 6 :
+        		System.out.println("Thank you! Good Bye!!");
+        	    System.exit(0);
+        	default :
+        		System.out.println("Please enter a correct choice!!!");
+        	}
+        	
+        }
+		
 
 	}
 
